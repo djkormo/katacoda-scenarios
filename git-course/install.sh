d9b0e6ac25d3d684cf3802917e60cf14a7378711
KP <djkormo@gmail.com>
2020-08-05 22:50:24 +0200

    adding run-command-02

--- git-course/install.sh
+++ git-course/install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 apt install --yes apache2 
+apt install --yes composer
---
+++ git-course/run-command-02-in-background.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd ~/demo
+
+touch ~/demo/index.html
+
+echo "<html><title>Title</title><body></body></html>" > ~/demo/index.html
+
+
