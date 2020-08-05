44206d7a1321ec01269752116a7ddba7523b225b
Krzysztof Pudłowski <kpudlowski@cyfrowypolsat.pl>
2020-08-05 15:37:23 +0200

    changing directory name

---
+++ git-course/010.git-init.verify.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+[ -d ~/demo ] && [ -d ~/demo/.git ] && [ -f ~/demo/.git/HEAD ] && echo "done"
+[ -d ~/tutorial/demo ] && [ -d ~/tutorial/demo/.git ] && [ -f ~/tutorial/demo/.git/HEAD ] && echo "done"
