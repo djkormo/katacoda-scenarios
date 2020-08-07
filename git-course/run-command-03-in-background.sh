35f50965c0949f82d6552455ee550d5aa4637356
KP <djkormo@gmail.com>
2020-08-07 18:43:29 +0200

    changing 03 content

--- git-course/run-command-03-in-background.sh
+++ git-course/run-command-03-in-background.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 cd ~/demo
+
 touch ~/demo/README
 echo "README VERSION 1" > ~/demo/README
+git add README
 
 echo "<html><title>Title 2</title><body></body></html>" > ~/demo/index.html
 
