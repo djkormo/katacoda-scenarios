3ff6d1f45494bf05be16a27baefbfe8c36cd4b1a
KP <djkormo@gmail.com>
2020-08-07 18:04:26 +0200

    adding more content 11

---
+++ git-course/020.git-init.verify.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+[ -d ~/demo ] && [ -d ~/demo/.gitignore ] && [ -f ~/demo/.git/HEAD ] && echo "done"
+[ -d ~/tutorial/demo ] && [ -d ~/tutorial/demo/.gitignore ] && [ -f ~/tutorial/demo/.git/HEAD] && echo "done"
