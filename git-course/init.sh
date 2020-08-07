968668b6da2460b57e89cb47e000a179d3aba145
KP <djkormo@gmail.com>
2020-08-07 22:08:04 +0200

    adding branch name in prompt

--- git-course/init.sh
+++ git-course/init.sh
@@ -4,7 +4,7 @@ cat <<EOF >~/.bashrc
 git_branch() {
  git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
 }
-export PS1="\[\033[1;36m\]\u\[\033[1;31m\]@\[\033[1;32m\]\h:\[\033[1;35m\]\w\[\033[1;31m\]\$\[\033[0m\]\$(git_branch)\$ "
+export PS1='\[\033[0;32m\]\[\033[0m\033[0;32m\]\u\[\033[0;36m\] @ \w\[\033[0;32m\] - [$(git branch 2>/dev/null | grep "^*" | colrm 1 2)\[\033[0;32m\]]\[\033[0m\033[0;32m\] \$\[\033[0m\033[0;32m\]\[\033[0m\] '
 EOF
 
 source ~/.bashrc
