7f8768f4989431d3f58886668579faa6fea43160
KP <djkormo@gmail.com>
2020-09-18 23:48:05 +0200

    more content

--- kubernetes-sandbox/install.sh
+++ kubernetes-sandbox/install.sh
@@ -2,6 +2,8 @@
 
 apt-get update
 
+apt-get install kubelet=1.17.0-00 kubeadm=1.17.0-00 kubectl=1.17.0-00
+
 cat <<EOF >~/.bashrc
 git_branch() {
  git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
@@ -10,3 +12,5 @@ export PS1="\[\033[1;36m\]\u\[\033[1;31m\]@\[\033[1;32m\]\h:\[\033[1;35m\]\w\[\0
 EOF
 
 source ~/.bashrc
+
+clear
