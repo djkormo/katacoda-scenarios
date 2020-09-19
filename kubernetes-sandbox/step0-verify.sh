4b6f022ab7c496c182d9a30ad707a94af12c24fb
KP <djkormo@gmail.com>
2020-09-19 22:18:30 +0200

    testing verify

--- kubernetes-sandbox/step0-verify.sh
+++ kubernetes-sandbox/step0-verify.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 [ -d ~/.kube/config ] && echo "done"
+kubectl get nodes | grep 1.18.0 | grep Ready | wc -l | grep 2 && echo "done"
--- kubernetes-sandbox/step1-verify.sh
+++ kubernetes-sandbox/step1-verify.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-echo "done"
+[ -d ~/.kube/config ] && echo "done"
+
+kubectl get nodes | grep 1.19.0 | grep Ready | wc -l | grep 2 && echo "done"
