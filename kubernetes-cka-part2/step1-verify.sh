3e11dd240c4e7388b59aee975620ea639f7d9b04
KP <djkormo@gmail.com>
2020-09-24 00:18:42 +0200

    adding initial cka part 2

--- kubernetes-cka-part2/step1-verify.sh
+++ kubernetes-cka-part2/step1-verify.sh
@@ -2,4 +2,7 @@
 #[ -d ~/.kube/config ] && echo "done"
 
 #kubectl get nodes | grep 1.19.0 | grep Ready | wc -l | grep 2 && echo "done"
-kubectl get svc web -n alpha |grep 80 && kubectl get svc web -n alpha |grep 443 &&  echo "done"
+kubectl get pod web -n alpha |grep Running \
+  && kubectl get pod web -n alpha -o yaml  |grep 'image: nginx:1.11.9-alpine' \
+  && kubectl get svc web -n alpha |grep 80 \
+  && kubectl get svc web -n alpha |grep 443 &&  echo "done"
