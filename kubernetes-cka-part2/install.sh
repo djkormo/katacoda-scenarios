4a5e5873b21841e2c76313fe3e16ece69ea995cf
KP <djkormo@gmail.com>
2020-09-23 22:32:33 +0200

    adding initial cka part 2

--- kubernetes-cka-part2/install.sh
+++ kubernetes-cka-part2/install.sh
@@ -6,12 +6,3 @@ apt-get update
 echo "Starting cluster"
 launch.sh
 echo "done" >> /opt/.clusterstarted
-
-cat <<EOF >~/.bashrc
-git_branch() {
- git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
-}
-export PS1="\[\033[1;36m\]\u\[\033[1;31m\]@\[\033[1;32m\]\h:\[\033[1;35m\]\w\[\033[1;31m\]\$\[\033[0m\]\$(git_branch)\$ "
-EOF
-
-source ~/.bashrc
