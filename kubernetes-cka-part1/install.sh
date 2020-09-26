15075d8d49632f26b08a59a5bcd0c40bd560d211
djkormo <djkormo@gmail.com>
2020-09-26 19:29:25 +0200

    polishing cka1 part

--- kubernetes-cka-part1/install.sh
+++ kubernetes-cka-part1/install.sh
@@ -18,13 +18,24 @@ set tabstop=2
 set autoindent
 EOF
 
-#cat <<EOF >~/.bashrc
-#git_branch() {
-# git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
-#}
+cat <<EOF >~/.bashrc
+git_branch() {
+ git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
+}
 
-#export PS1="\[\033[1;36m\]\u\[\033[1;31m\]@\[\033[1;32m\]\h:\[\033[1;35m\]\w\[\033[1;31m\]\$\[\033[0m\]\$(git_branch)\$ "
-#EOF
+export PS1="\[\033[1;36m\]\u\[\033[1;31m\]@\[\033[1;32m\]\h:\[\033[1;35m\]\w\[\033[1;31m\]\$\[\033[0m\]\$(git_branch)\$ "
+EOF
+
+cat <<EOF >~/.bashrc
+free_time()
+{
+    #awk "{print $1}" /proc/uptime
+    uptime | grep -ohe 'up .*' | sed 's/,//g' | awk '{ print $2" "$3 }'
+}
+GREEN="\[$(tput setaf 2)\]"
+RESET="\[$(tput sgr0)\]"
+export PS1="$(free_time) ${GREEN}\u@\h ${RESET} :"
+EOF
 
 source ~/.bashrc
 
