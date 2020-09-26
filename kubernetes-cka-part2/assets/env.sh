a66d0abd21eeb14dc5b83bbe3c01d5e015050ade
djkormo <djkormo@gmail.com>
2020-09-26 13:12:15 +0200

    changing prompt

--- kubernetes-cka-part2/assets/env.sh
+++ kubernetes-cka-part2/assets/env.sh
@@ -8,3 +8,17 @@ set paste
 set tabstop=2
 set autoindent
 EOF
+
+free_mem()
+{
+    awk '/MemFree/{print $2}' /proc/meminfo
+}
+
+free_time()
+{
+    awk '/MemFree/{print $2}' /proc/meminfo
+}
+
+
+PS1="${GREEN}my prompt${RESET}> "
+PS1="${GREEN}\u@\h$ $(free_time) {RESET}> 
