922e81fe02952a53df7c042ec453fcc06dc27e33
djkormo <djkormo@gmail.com>
2020-09-26 13:32:44 +0200

    changing prompt

--- kubernetes-cka-part2/assets/env.sh
+++ kubernetes-cka-part2/assets/env.sh
@@ -9,19 +9,3 @@ set tabstop=2
 set autoindent
 EOF
 
-# https://www.howtogeek.com/307701/how-to-customize-and-colorize-your-bash-prompt/
-# https://wiki.archlinux.org/index.php/Bash/Prompt_customization
-
-free_mem()
-{
-    awk '/MemFree/{print $2}' /proc/meminfo
-}
-
-free_time()
-{
-    uptime -p 
-}
-
-GREEN="\[$(tput setaf 2)\]"
-RESET="\[$(tput sgr0)\]"
-PS1="$(free_time) ${GREEN}\u@\h ${RESET}:"
--- kubernetes-cka-part2/assets/wait.sh
+++ kubernetes-cka-part2/assets/wait.sh
@@ -41,3 +41,21 @@ showProgress()
 }
 
 showProgress
+
+# https://www.howtogeek.com/307701/how-to-customize-and-colorize-your-bash-prompt/
+# https://wiki.archlinux.org/index.php/Bash/Prompt_customization
+
+free_mem()
+{
+    awk '/MemFree/{print $2}' /proc/meminfo
+}
+
+free_time()
+{
+    uptime -p 
+}
+
+GREEN="\[$(tput setaf 2)\]"
+RESET="\[$(tput sgr0)\]"
+PS1="$(free_time) ${GREEN}\u@\h ${RESET}:"
+
