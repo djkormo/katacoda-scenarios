4942c716c9b159202ac56acf9859471b518a97a8
djkormo <djkormo@gmail.com>
2020-10-10 22:00:26 +0200

    show visualize in cka3 - networking

--- kubernetes-cka-part3/install.sh
+++ kubernetes-cka-part3/install.sh
@@ -20,12 +20,20 @@ set autoindent
 EOF
 
 
-curl -L https://cloud.weave.works/launch/k8s/weavescope.yaml
+#curl -L https://cloud.weave.works/launch/k8s/weavescope.yaml
 
-kubectl apply -f https://cloud.weave.works/launch/k8s/weavescope.yaml
+#kubectl apply -f https://cloud.weave.works/launch/k8s/weavescope.yaml
+
+
+kubectl apply -f https://raw.githubusercontent.com/djkormo/katacoda-scenarios/master/kubernetes-cka-part3/step1/weavescope-deploy.yaml -n weave
+kubectl apply -f https://raw.githubusercontent.com/djkormo/katacoda-scenarios/master/kubernetes-cka-part3/step1/weavescope-service.yaml -n weave
+
 
 pod=$(kubectl get pod -n weave --selector=name=weave-scope-app -o jsonpath={.items..metadata.name})
 
+kubectl expose pod $pod -n weave --name=weave-svc --external-ip="172.17.0.34" --port=4040 --target-port=4040
 
-kubectl expose pod $pod -n weave --name=weave-nodeport --external-ip="172.17.0.34" --port=4040 --target-port=4040 --nodeport=4000
+kubectl port-forward $pod  4040:4040  --address 0.0.0.0 -n weave &
+
+# kubectl expose pod $pod -n weave --name=weave-nodeport --external-ip="172.17.0.34" --port=4040 --target-port=4040
 
