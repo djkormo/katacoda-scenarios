be997144ea7fb3a914b07949243538766125c77b
djkormo <djkormo@gmail.com>
2020-10-10 22:34:51 +0200

    show visualize in cka3 - networking

--- kubernetes-cka-part3/install.sh
+++ kubernetes-cka-part3/install.sh
@@ -30,10 +30,10 @@ kubectl apply -f https://raw.githubusercontent.com/djkormo/katacoda-scenarios/ma
 
 
 pod=$(kubectl get pod -n weave --selector=name=weave-scope-app -o jsonpath={.items..metadata.name})
+#kubectl port-forward $pod  4040:4040  --address 0.0.0.0 -n weave & >dev/null
 
-kubectl expose pod $pod -n weave --name=weave-svc --external-ip="172.17.0.34" --port=4040 --target-port=4040
-
-kubectl port-forward $pod  4040:4040  --address 0.0.0.0 -n weave &
+kubectl expose pod $pod -n weave --name=weave-svc --external-ip=[[HOST_IP]] --port=4040 --target-port=4040
+#kubectl port-forward $pod  4040:4040  --address 0.0.0.0 -n weave &
 
 # kubectl expose pod $pod -n weave --name=weave-nodeport --external-ip="172.17.0.34" --port=4040 --target-port=4040
 
