89b8cb127529c8563f781a41e55bfcce1ec8b5f8
djkormo <djkormo@gmail.com>
2020-10-17 23:12:39 +0200

    polishing content

--- kubernetes-cka-part1/install.sh
+++ kubernetes-cka-part1/install.sh
@@ -4,37 +4,7 @@ apt-get update
 
 # Start Kubernetes
 echo "Starting cluster"
-# launch.sh
 
-
-# create a vimrc
-#cat <<EOF >>.vimrc
-#" Created on $(date)
-#set nocompatible
-#syntax enable
-#filetype plugin indent on
-#set paste
-#set tabstop=2
-#set autoindent
-#EOF
-
-#cat <<EOF >~/.bashrc
-#git_branch() {
-# git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
-#}
-
-#export PS1="\[\033[1;36m\]\u\[\033[1;31m\]@\[\033[1;32m\]\h:\[\033[1;35m\]\w\[\033[1;31m\]\$\[\033[0m\]\$(git_branch)\$ "
-#EOF
-
-#cat <<EOF >~/.bashrc
-#free_time()
-#{
-#    uptime | grep -ohe 'up .*' | sed 's/,//g' | awk '{ print $2" "$3 }'
-#}
-#GREEN="\[$(tput setaf 2)\]"
-#RESET="\[$(tput sgr0)\]"
-#export PS1="$(free_time) ${GREEN}\u@\h ${RESET} :"
-#EOF
 
 source ~/.bashrc
 
