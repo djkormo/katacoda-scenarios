8eb017bafe4e4f8e6fe81a0233ebb84033e6cfa8
djkormo <djkormo@gmail.com>
2020-10-17 19:15:13 +0200

    polishing cntent

--- kubernetes-cka-part5/foreground.sh
+++ kubernetes-cka-part5/foreground.sh
@@ -1,6 +1,6 @@
 echo off
 sleep 10; bash /usr/local/bin/wait.sh
-
+clear
 free_time()
 {
     uptime | grep -ohe 'up .*' | sed 's/,//g' | awk '{ print 60-$2" "$3 }'
@@ -12,8 +12,7 @@ prompt() {
 export PS1="$(free_time) \[\033[1;36m\]\u\[\033[1;31m\]@\[\033[1;32m\]\h:\[\033[1;35m\]\w\[\033[1;31m\]\$\[\033[0m\]\$(git_branch)\$ "
 }
 PROMPT_COMMAND=prompt
-echo "Almost ready!!!"
-
+clear
 sleep 10;
 source ~/.bashrc
 
