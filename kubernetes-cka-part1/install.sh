e96ef9f6c11fe3b1e9a590733893fda9089ffe1a
djkormo <djkormo@gmail.com>
2020-10-18 16:38:08 +0200

    polishing content

--- kubernetes-cka-part1/install.sh
+++ kubernetes-cka-part1/install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo off
-apt-get update
+apt-get update && apt-get install kubeadm=1.18.0-00 kubelet 1.18.0-00 kubectl=1.18.0-00 -y && systemctl restart kubelet
+
+ssh root@[[HOST2_IP]] "apt-get update && apt-get install kubeadm=1.18.0-00 kubelet=1.18.0-00 -y && systemctl restart kubelet"  >> /var/log/install
 
 # Start Kubernetes
 echo "Starting cluster"
