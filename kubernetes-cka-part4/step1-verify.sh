f4939ae92cef47f6164a6f32ac6f7bbe0310050f
KP <djkormo@gmail.com>
2020-11-28 09:45:45 +0100

    changing conditional

--- kubernetes-cka-part4/step1-verify.sh
+++ kubernetes-cka-part4/step1-verify.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 
-curl -sSf http://localhost:30001 > /tmp/alpha.log
-
-kubectl get svc frontend -n alpha -o yaml |grep "nodePort: 30001" \
+curl -sSf http://localhost:30001 > /tmp/alpha.log \
+&& kubectl get svc frontend -n alpha -o yaml |grep "nodePort: 30001" \
 && cat /tmp/alpha.log | grep "<title>Guestbook</title>" \
 &&  echo "done"
