e8899994b8cc3658a93bb1cd322d17327861d82f
djkormo <djkormo@gmail.com>
2020-11-30 22:00:47 +0100

    adding more content

--- kubernetes-cka-part4b/step1-background.sh
+++ kubernetes-cka-part4b/step1-background.sh
@@ -14,13 +14,16 @@ kubectl create ns alpha
 
 ## Installing metrics server 
 
-kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/download/v0.3.7/components.yaml
+#kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/download/v0.3.7/components.yaml
 
 
-sleep 1
-cd /manifests
-ls
-kubectl run fix-me --image=nginx:3
-kubectl apply -f nginx-deployment.yml
-clear
+#sleep 1
+#cd /manifests
+#ls
+#kubectl run fix-me --image=nginx:3
+#kubectl apply -f nginx-deployment.yml
+#clear
+
+
+
 
