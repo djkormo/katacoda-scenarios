da208617cebc1643ba71fd77281afd710fed3564
Krzysztof Pudłowski <kpudlowski@cyfrowypolsat.pl>
2020-12-04 12:33:10 +0100

    refactoring

--- kubernetes-cka-part4b/step1-background.sh
+++ kubernetes-cka-part4b/step1-background.sh
@@ -10,13 +10,12 @@ echo "done" >> /opt/.clusterstarted
 ## Installing metrics server 
 
 
-git clone https://github.com/vocon-it/metrics-server
+git clone https://github.com/vocon-it/metrics-server >>/var/log/step1-background.log
+kubectl apply -f ./metrics-server/deploy/1.8+/ >>/var/log/step1-background.log
+kubectl create ns alpha >>/var/log/step1-background.log
+kubectl run fix-me --image=nginx:3 -n alpha >>/var/log/step1-background.log
 
-kubectl apply -f ./metrics-server/deploy/1.8+/
 
-kubectl create ns alpha
-
-kubectl run fix-me --image=nginx:3 -n alpha
 
 
 
