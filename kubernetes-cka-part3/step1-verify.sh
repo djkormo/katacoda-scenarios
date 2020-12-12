359e66687385292f3a6c4e326451a2031ef20770
djkormo <djkormo@gmail.com>
2020-12-12 14:33:56 +0100

    Correcting content

--- kubernetes-cka-part3/step1-verify.sh
+++ kubernetes-cka-part3/step1-verify.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
+
+mkdir -p /var/log/verify
+
 master_node=$(kubectl get node -l node-role.kubernetes.io/master= -o name)
 
 kubectl get $master_node -o wide | awk '{print $6}' | grep -v INTERNAL > /var/log/verify/07-ip-master.txt
 
 ip link show ens3 | awk '{print $2}' |grep -v ens3 > /var/log/verify/08-mac-master.txt
+
 
 echo "done" 
