f03fffb0d5cf229234211043fa0da5f810f62ba0
djkormo <djkormo@gmail.com>
2020-12-12 14:14:25 +0100

    Correcting content

--- kubernetes-cka-part3/step1-verify.sh
+++ kubernetes-cka-part3/step1-verify.sh
@@ -4,8 +4,12 @@ master_node=$(kubectl get node -l node-role.kubernetes.io/master= -o name)
 kubectl get $master_node -o wide
 kubectl get $master_node -o wide | awk '{print $6}'
 kubectl get $master_node -o wide | awk '{print $6}' | grep -v INTERNAL
-kubectl get $master_node -o wide | awk '{print $6}' | grep -v INTERNAL > /var/log/answers/07-ip-master.txt
+kubectl get $master_node -o wide | awk '{print $6}' | grep -v INTERNAL > /var/log/verify/07-ip-master.txt
 
-cat /var/log/answers/07-ip-master.txt
+
+ip link show ens3
+ip link show ens3 | awk '{print $1}'
+ip link show ens3 | awk '{print $2}' |grep -v ens3
+ip link show ens3 | awk '{print $2}' |grep -v ens3 > /var/log/verify/08-mac-master.txt
 
 echo "done" 
