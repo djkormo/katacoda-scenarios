47169401195b61bf8adf29c0a9515680b62d4618
Krzysztof Pudłowski <kpudlowski@cyfrowypolsat.pl>
2020-12-22 15:18:15 +0100

    refactoring

--- kubernetes-cka-part2/install.sh
+++ kubernetes-cka-part2/install.sh
@@ -45,3 +45,8 @@ install-krew.sh >> /opt/.krewinstall
 export PATH="${PATH}:${HOME}/.krew/bin" >> /opt/.krewinstall
 
 echo "done" >> /opt/.krewinstall
+
+## Installing metrics server 
+git clone https://github.com/vocon-it/metrics-server >>/var/log/step1-background.log
+kubectl apply -f ./metrics-server/deploy/1.8+/ >>/var/log/step1-background.log
+
