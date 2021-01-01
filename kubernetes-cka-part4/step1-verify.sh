06eaec98e2c48bb66a90f4a8326cdbca45867d1b
Krzysztof Pudłowski <kpudlowski@plus.pl>
2021-01-01 11:03:14 +0100

    disabling verify

--- kubernetes-cka-part4/step1-verify.sh
+++ kubernetes-cka-part4/step1-verify.sh
@@ -5,3 +5,6 @@ curl -sSf http://localhost:30001 > /tmp/alpha.log \
 && kubectl get svc frontend -n alpha -o yaml |grep "nodePort: 30001" \
 && cat /tmp/alpha.log | grep "<title>Guestbook</title>" \
 &&  echo "done"
+  
+
+  
