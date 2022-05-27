3154c31893fc868570964fc51021077b8399b99e
Krzysztof Pudłowski <djkormo@gmail.com>
2022-05-27 15:40:15 +0200

    removing metrics server

--- kubernetes-cka-part4/install.sh
+++ kubernetes-cka-part4/install.sh
@@ -63,5 +63,5 @@ kubectl create ns alpha
 
 
 ## Installing metrics server 
-git clone https://github.com/vocon-it/metrics-server >>/var/log/step1-background.log
-kubectl apply -f ./metrics-server/deploy/1.8+/ >>/var/log/step1-background.log
+#git clone https://github.com/vocon-it/metrics-server >>/var/log/step1-background.log
+#kubectl apply -f ./metrics-server/deploy/1.8+/ >>/var/log/step1-background.log
