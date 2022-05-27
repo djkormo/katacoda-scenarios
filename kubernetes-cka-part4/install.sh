c27b9b949a6b638b9a26ceac6362f12026e01717
Krzysztof Pudłowski <djkormo@gmail.com>
2022-05-27 15:33:08 +0200

    Update install.sh

--- kubernetes-cka-part4/install.sh
+++ kubernetes-cka-part4/install.sh
@@ -9,9 +9,9 @@ echo "done" >> /opt/.clusterstarted
 
 
 apt-get update >> /var/log/install
-apt-get install kubeadm=1.19.0-00 kubectl=1.19.0-00 kubelet=1.19.0-00 -y  >> /var/log/install
+apt-get install kubeadm=1.24.0-00 kubectl=1.24.0-00 kubelet=1.24.0-00 -y  >> /var/log/install
 systemctl restart kubelet  >> /var/log/install
-kubeadm upgrade apply v1.19.0 -y  >> /var/log/install
+kubeadm upgrade apply v1.24.0 -y  >> /var/log/install
 systemctl restart kubelet  >> /var/log/install
 
 echo "Upgrading cluster"
@@ -20,7 +20,7 @@ echo "done" >> /opt/.clusterupgraded
 date >> /opt/.clusterupgraded
 
 echo "Upgrading node"
-ssh root@[[HOST2_IP]] "apt-get update && apt-get install kubeadm=1.19.0-00 kubelet=1.19.0-00 -y && systemctl restart kubelet"  >> /var/log/install
+ssh root@[[HOST2_IP]] "apt-get update && apt-get install kubeadm=1.24.0-00 kubelet=1.24.0-00 -y && systemctl restart kubelet"  >> /var/log/install
 
 echo "done" >> /opt/.nodeupgraded
 date >> /opt/.nodeupgraded
