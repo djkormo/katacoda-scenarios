507b0943da1d8e09698f459771df479943a2f3c9
Krzysztof Pudłowski <djkormo@gmail.com>
2022-05-28 13:13:47 +0200

    Update install.sh

--- kubernetes-cka-part4/install.sh
+++ kubernetes-cka-part4/install.sh
@@ -70,6 +70,6 @@ kubectl create namespace alpha --dry-run=client -o yaml | kubectl apply -f -
 # Documented here 
 # https://thospfuller.com/2020/11/29/easy-kubernetes-metrics-server-install-in-minikube-in-five-steps/
 
-kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
+#kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
 kubectl apply -f https://gist.githubusercontent.com/thospfuller/d0d918e0b9fdb719a34d3d355b0886bb/raw/63fefb2d1cf31563db04c60f036d6661b5ec5ff2/components.yaml
 
