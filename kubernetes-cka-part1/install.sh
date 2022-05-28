b267b4753f2526ff2c4ced809338ddecbc4320cb
djkormo <djkormo@gmail.com>
2022-05-28 13:49:57 +0200

    changing imageid

--- kubernetes-cka-part1/install.sh
+++ kubernetes-cka-part1/install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 echo off
-apt-get update && apt-get install kubeadm=1.23.0-00 kubelet 1.23.0-00 kubectl=1.23.0-00 -y && systemctl restart kubelet
+apt-get update && apt-get install kubeadm=1.23.0-00 kubelet 1.23.0-00 kubectl=1.23.0-00 -y  >> /var/log/install
+systemctl restart kubelet >> /var/log/install
 
-ssh root@[[HOST2_IP]] "apt-get update && apt-get install kubeadm=1.23.0-00 kubelet=1.23.0-00 -y && systemctl restart kubelet"  >> /var/log/install
+ssh root@[[host2]] "apt-get update && apt-get install kubeadm=1.23.0-00 kubelet=1.23.0-00 -y && systemctl restart kubelet"  >> /var/log/install
 
 # Start Kubernetes
 echo "Starting cluster"
-
 
 source ~/.bashrc
 
